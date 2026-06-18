ff2a47f493255419c8a65c6e5da7eabf3dae508c
saifalihdd <saifalialhaddad2@gmail.com>
2026-06-18 23:49:49 +0700

    feat: implement report submission and detail view

--- infravision-web/src/pages/SubmitReportPage.jsx
+++ infravision-web/src/pages/SubmitReportPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../lib/axios";
-
+ 
 const CATEGORIES = [
   "Jalan Berlubang",
   "Retak Jalan/Bangunan",
